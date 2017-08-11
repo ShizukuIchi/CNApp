18dd0c31714f9640d8c4c85e9cb88026c790516f
shizuku <eddy540511@yahoo.com.tw>
2017-08-11 16:49:14 +0800

    update settings

--- CNA/settings.py
+++ CNA/settings.py
@@ -110,6 +110,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
